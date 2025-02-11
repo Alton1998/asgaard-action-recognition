1bd67c8b1e4956adfd7b5e17002d36a5b7adf64c
Alton1998 <altondsouza02@gmail.com>
2025-02-10 17:36:15 -0700

    Fix

--- workspace/custom_shot_boundary_detector/key_shot_annotator_classifier.py
+++ workspace/custom_shot_boundary_detector/key_shot_annotator_classifier.py
@@ -3,16 +3,15 @@ import cv2
 from PIL import Image
 from transformers import LlavaProcessor, LlavaForConditionalGeneration
 import torch
-from openpyxl import Workbook
-from openpyxl.drawing.image import Image as XLImage
 import argparse
+import csv
 from tqdm import tqdm
 import os
 
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
-def annotate_key_shots(key_shots,video_path,model_id="llava-hf/llava-interleave-qwen-0.5b-hf",user_prompt = "What is this scene about?"):
+def annotate_key_shots(key_shots,video_path,model_id="llava-hf/llava-interleave-qwen-7b-hf",user_prompt = "What is this scene about?"):
     video = cv2.VideoCapture(video_path)
     total_frames = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
     processor = LlavaProcessor.from_pretrained(model_id)
@@ -32,20 +31,18 @@ def annotate_key_shots(key_shots,video_path,model_id="llava-hf/llava-interleave-
             output_decoded = processor.decode(output[0][2:], skip_special_tokens=True)[len(user_prompt)+10:]
             yield (pil_image,output_decoded)
 
-def write_to_sheets(title,image_list,output_path):
-    wb = Workbook()
-    ws = wb.active
-    ws.title = title
-    ws.append(["Image", "Description"])
-    for i, (img, desc) in tqdm(enumerate(image_list, start=2),desc="Annotating"): 
-        img_path = f"image_{i-1}.png"
-        img.save(img_path)  
-
-        ws[f"B{i}"] = desc
-        excel_img = XLImage(img_path)
-        ws.add_image(excel_img, f"A{i}")
-    wb.save(output_path)
-
+def write_to_csv(image_list, output_path):
+    os.makedirs(output_path, exist_ok=True)
+    i = 0
+    with open(output_path+"_annotated.csv", mode='w', newline='', encoding='utf-8') as file:
+        writer = csv.writer(file)
+        writer.writerow(["Image Path", "Description"])
+        for img, desc in tqdm(image_list,desc="Annotating key shots"):
+            image_path = os.path.join(output_path, f"Key_shot_{i}.png")
+            img.save(image_path)
+            writer.writerow([image_path, desc])
+            i = i+1
+            
 if __name__=="__main__":
     parser = argparse.ArgumentParser(description="Key Shot Annotation")
     parser.add_argument('--csv', type=str, help="CSV Path")
@@ -53,10 +50,21 @@ if __name__=="__main__":
     args = parser.parse_args()
     video_path = args.video
     csv_path = args.csv
-    key_shots = pd.read_csv("1_shots.csv").values.reshape(1,-1)[0].tolist()
-    image_list = annotate_key_shots(key_shots=key_shots,video_path=video_path)
+    key_shots = pd.read_csv(csv_path).values.reshape(1,-1)[0].tolist()
+    user_prompt = """
+    Classify the given scene into the following:
+1. Rescue
+2. Escape
+3. Capture
+4. Heist
+5. Fight
+6. Pursuit
+7. None of the Above - For scenes that do not fall into any of the following categories.
+    """
+    image_list = annotate_key_shots(key_shots=key_shots,video_path=video_path,user_prompt=user_prompt)
     base_name = os.path.splitext(os.path.basename(video_path))[0]
-    write_to_sheets(base_name,image_list,base_name+".xlsx")
+    write_to_csv(image_list,base_name)
+    
     
 
 
